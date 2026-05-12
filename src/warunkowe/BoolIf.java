b1a57cc529ff34555ae6dc856fc7a18796f38c6b
Przemek Janisz <p.janisz@ckp-ns.edu.pl>
2026-05-12 08:03:32 +0200

    Commit nr2

---
+++ src/warunkowe/BoolIf.java
@@ -0,0 +1,15 @@
+package warunkowe;
+
+public class BoolIf {
+    public static void main(String[] args) {
+        boolean sun = true;
+
+
+        if(!sun){
+            System.out.println("Jest ładnie");
+        }
+        else{
+            System.out.println("Jest brzydko");
+        }
+    }
+}
